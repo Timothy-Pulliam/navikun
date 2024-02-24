4742926c20192263881b7b4a5a6b20ef69647c68
Timothy Pulliam <timothy.dpulliam@gmail.com>
2024-02-24 13:56:50 -0500

    add Terraform

--- README.md
+++ README.md
@@ -5,7 +5,21 @@ docker build -t navi-kun:latest .
 docker run -d -p 8080:8080 navi-kun:latest
 ```
 
-# Deploy Infrastructure via Cloudformation
+# Estimating Costs with Infracost/Terraform
+
+Install Infracost
+
+```
+brew install infracost
+```
+
+Run a cost breakdown
+
+```
+infracost breakdown --path ./Terraform
+```
+
+# Deploy Infrastructure via Cloudformation/Terraform
 
 ```bash
 aws cloudformation create-stack --stack-name my-vpc-ecr-stack --template-body file://infra.yaml --capabilities CAPABILITY_IAM
---
+++ Terraform/main.tf
@@ -0,0 +1,144 @@
+terraform {
+  # Terraform Version
+  required_version = ">= 1.7.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_lightsail_instance" "my_micro_linux_instance" {
+  name              = "MyMicroLinuxInstance"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "micro_amazon_linux_2" # This is a placeholder value
+  bundle_id         = "micro_2_0"            # Adjust this to the specific bundle for a micro instance
+
+  tags = {
+    "Name" = "MyMicroLinuxInstance"
+  }
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+provider "aws" {
+  # Define your provider configuration here (e.g., region)
+
+
+}
+
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-navikun-dev"
+    env  = "dev"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "ig-navikun-dev"
+    env  = "dev"
+  }
+}
+
+resource "aws_subnet" "this" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "snet-navikun-dev"
+    env  = "dev"
+  }
+}
+
+resource "aws_security_group" "this" {
+  vpc_id = aws_vpc.this.id
+
+  description = "Allow HTTP and HTTPS inbound traffic"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecr_repository" "this" {
+  name = "ecr-navikun-dev"
+  # The repository policy can be defined here if necessary.
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "s3-navikun-dev"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  public_access_block_configuration {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+}
+
+output "VPCId" {
+  value = aws_vpc.this.id
+}
+
+output "SubnetId" {
+  value = aws_subnet.this.id
+}
+
+output "SecurityGroupId" {
+  value = aws_security_group.this.id
+}
+
+output "ECRRepositoryARN" {
+  value = aws_ecr_repository.this.arn
+}
+
+output "ECRRepositoryName" {
+  value = aws_ecr_repository.this.name
+}
+
+output "BucketName" {
+  value = aws_s3_bucket.this.bucket
+}
+
+output "BucketURL" {
+  value = aws_s3_bucket.this.bucket_domain_name
+}
